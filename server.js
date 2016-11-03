110aaa4a0ac60751cdd3a3211b6ac25a0b5545f5
hangilc <hangil@chang.jp>
2016-11-03 16:35:11 +0900

    added commander

--- server.js
+++ server.js
@@ -3,13 +3,29 @@
 var app = require("./index");
 var Config = require("myclinic-config");
 var path = require("path");
+var program = require("commander");
 
-var config = {
-	port: 9001,
-	"service-url": "http://localhost:9000/service"
-};
+program
+	.option("-c, --config <configpath>", "Read configuration")
+	.option("-s, --service <service-url>", "Set service server")
+	.option("-p, --port <port>", "Set listening port")
+	.parse(process.argv);
 
-Config.extend(config, Config.read(path.join(process.env.MYCLINIC_CONFIG, "practice")));
+var config = {};
+
+if( program.config ){
+	Config.extend(config, Config.read(program.config));
+} else {
+	Config.extend(config, Config.read(path.join(process.env.MYCLINIC_CONFIG, "practice")));
+}
+if( program.service ){
+	config["service-url"] = program.service;
+} else {
+	config["service-url"] = process.env.MYCLINIC_SERVER;
+}
+if( program.port ){
+	config.port = program.port;
+}
 
 app.run(config);
 
