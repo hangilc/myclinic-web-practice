01510033e58afc6a902ab1c3d9c2ac6308b2cd89
hangilc <hangil@chang.jp>
2016-11-06 22:16:42 +0900

    augmented printer-settings

--- run.example.bat
+++ run.example.bat
@@ -1,1 +1,2 @@
-node server -c "C:\myclinic-configuration" -s "http://localhost:9000/service" -p 9001
+node server -c "C:\myclinic-configuration" -s "http://localhost:9000/service" -p 9001 --printer-settings "./printer-settings"
+
--- server.js
+++ server.js
@@ -4,11 +4,28 @@ var app = require("./index");
 var Config = require("myclinic-config");
 var path = require("path");
 var program = require("commander");
+var fs = require("fs");
+var mkdirp = require("mkdirp");
+
+function toInt(val){
+	return parseInt(val, 10);
+}
+
+function ensureDir(pathname){
+	if( !pathname ){
+		return;
+	}
+	if( !fs.existsSync(pathname) ){
+		console.log("creating directory:", pathname);
+		mkdirp.sync(pathname);
+	}
+}
 
 program
 	.option("-c, --config <configpath>", "Read configuration", process.env.MYCLINIC_CONFIG)
 	.option("-s, --service <service-url>", "Set service server", "http://localhost:9000/service")
-	.option("-p, --port <port>", "Set listening port", 9001)
+	.option("-p, --port <port>", "Set listening port", toInt, 9001)
+	.option("--printer-settings <dirname>", "Set printer settings directory", "./printer-settings")
 	.parse(process.argv);
 
 var config = Config.read(program.config);
@@ -16,6 +33,11 @@ config._web = {
 	"service-url": program.service,
 	"port": program.port
 };
+config.printer = {
+	"setting-dir": program.printerSetting
+};
+
+ensureDir(config.printer["setting-dir"]);
 
 app.run(config);
 
